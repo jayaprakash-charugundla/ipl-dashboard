26ab634c7822e9c10bbbdc1bc455ebdcad0d6f7f
Jayaprakash <jayaprakashc68@gmail.com>
2021-04-23 15:04:49 +0530

    Initial Batch

---
+++ src/main/java/com/jc/ipldashboard/data/MatchDataProcessor.java
@@ -0,0 +1,48 @@
+package com.jc.ipldashboard.data;
+
+import com.jc.ipldashboard.model.Match;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+
+import java.time.LocalDate;
+
+@Slf4j
+public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
+
+    @Override
+    public Match process(final MatchInput matchInput) {
+        Match match = new Match();
+        match.setId(Long.valueOf(matchInput.getId()));
+        match.setCity(matchInput.getCity());
+        match.setDate(LocalDate.parse(matchInput.getDate()));
+        match.setPlayerOfMatch(matchInput.getPlayer_of_match());
+        match.setVenue(matchInput.getVenue());
+
+        String firstInningsTeam, secondInningsTeam;
+
+        if ("bat".equals(matchInput.getToss_decision())) {
+            firstInningsTeam = matchInput.getToss_winner();
+            secondInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1())
+                    ? matchInput.getTeam2() : matchInput.getTeam1();
+        } else {
+            secondInningsTeam = matchInput.getToss_winner();
+            firstInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1())
+                    ? matchInput.getTeam2() : matchInput.getTeam1();
+        }
+
+        match.setTeam1(firstInningsTeam);
+        match.setTeam2(secondInningsTeam);
+
+        match.setTossWinner(matchInput.getToss_winner());
+        match.setTossDecision(matchInput.getToss_decision());
+        match.setResult(matchInput.getResult());
+        match.setResultMargin(matchInput.getResult_margin());
+        match.setUmpire1(matchInput.getUmpire1());
+        match.setUmpire2(matchInput.getUmpire2());
+
+        return match;
+    }
+
+}
