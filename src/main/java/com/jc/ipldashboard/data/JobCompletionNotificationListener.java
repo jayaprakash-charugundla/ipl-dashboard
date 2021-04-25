0270bb9d38be16c9a3daf7ae0acf881e271afe21
Jayaprakash <jayaprakashc68@gmail.com>
2021-04-25 18:38:45 +0530

    creating teams

--- src/main/java/com/jc/ipldashboard/data/JobCompletionNotificationListener.java
+++ src/main/java/com/jc/ipldashboard/data/JobCompletionNotificationListener.java
@@ -1,6 +1,6 @@
 package com.jc.ipldashboard.data;
 
-import com.jc.ipldashboard.model.Match;
+import com.jc.ipldashboard.model.Team;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
@@ -10,27 +10,63 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
 
     private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
 
     private final JdbcTemplate jdbcTemplate;
+    private final EntityManager em;
 
     @Autowired
-    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
+    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate, EntityManager em) {
         this.jdbcTemplate = jdbcTemplate;
+        this.em = em;
     }
 
+    @Transactional
     @Override
     public void afterJob(JobExecution jobExecution) {
-        if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
+        if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
             log.info("!!! JOB FINISHED! Time to verify the results");
 
-            jdbcTemplate.query("SELECT team1, team2, date FROM match",
+           /* jdbcTemplate.query("SELECT team1, team2, date FROM match",
                     (rs, row) -> "Team 1 " + rs.getString(1) + " Team 2 " + rs.getString(2) +
                             " Date " + rs.getString(3)
-            ).forEach(System.out::println);
+            ).forEach(System.out::println);*/
+
+            Map<String, Team> teamData = new HashMap<>();
+
+            em.createQuery("select m.team1, count(*) from Match m group by m.team1", Object[].class)
+                    .getResultList()
+                    .stream()
+                    .map(e -> new Team((String) e[0], (long) e[1]))
+                    .forEach(team -> teamData.put(((Team) team).getTeamName(), team));
+
+            em.createQuery("select m.team2, count(*) from Match m group by m.team2", Object[].class)
+                    .getResultList()
+                    .stream()
+                    .forEach(e -> {
+                        Team team = teamData.get((String) e[0]);
+                        team.setTotalMatches(team.getTotalMatches() + (long) e[1]);
+                    });
+
+            em.createQuery("select m.matchWinner, count(*) from Match m group by m.matchWinner", Object[].class)
+                    .getResultList()
+                    .stream()
+                    .forEach(e -> {
+                        Team team = teamData.get((String) e[0]);
+                        if (team != null) {
+                            team.setTotalWins((long) e[1]);
+                        }
+                    });
+            teamData.values().forEach(em::persist);
+            teamData.values().forEach(System.out::println);
         }
     }
 }
---
+++ src/main/java/com/jc/ipldashboard/model/Team.java
@@ -0,0 +1,32 @@
+package com.jc.ipldashboard.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String teamName;
+    private long totalMatches;
+    private long totalWins;
+
+    public Team(String teamName, long totalMatches) {
+        this.teamName = teamName;
+        this.totalMatches = totalMatches;
+    }
+
+}
